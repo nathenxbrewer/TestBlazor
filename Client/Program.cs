3d9eb25c900130adf387bb7358ac708a4c356c30
nathenxbrewer <nathenbrewer@gmail.com>
2022-04-05 22:55:29 -0400

    Adding MudBlazor and LocalStorage

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
+using MudBlazor.Services;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddBlazoredLocalStorage();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
---
+++ Shared/Models/Bill.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Resume.WASM.MoneyForcaster.Models
+{
+    public class Bill
+    {
+        public bool isActive { get; set; } = true;
+        public string Name { get; set; }
+        public DateTime BeginDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int DayOfMonth { get; set; }
+        public float Amount { get; set; }
+        public int OccuranceDays { get; set; } = 30;
+        public string Notes { get; set; }
+    }
+}
